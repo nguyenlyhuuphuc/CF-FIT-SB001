1a70366e3430818819500686506a671eac8c4082
Phuc Nguyen <nguyenlyhuuphucwork@gmail.com>
2024-08-17 17:09:44 +0700

    Blade Template

--- routes/web.php
+++ routes/web.php
@@ -6,31 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', function (){
-    return view('test.abc.xyz.test');
+Route::get('home', function (){
+    return view('client.pages.home');
 });
 
-Route::get('abc', function(){
-    echo '<h1>ABC</h1>';
+Route::get('contact', function (){
+    return view('client.pages.contact');
 });
 
-Route::get('a/b/c/x/y/z', function (){
-    echo '<i>Z</i>';
-});
-
-Route::get('test-sum', function (){
-    $number1 = 10;
-    $number2 = 5;
-
-    echo sprintf('%s + %s = %s', $number1, $number2, $number1 + $number2);
-});
-
-
-Route::get('product/detail/{productId}', function ($productId){
-    echo "Product id : $productId";
-});
-
-
-Route::get('product/{productId}/qty/{qty?}', function ($productId, $qty = 1){
-    echo sprintf("Add product Id : %s, into card with qty : %s", $productId, $qty);
-});
